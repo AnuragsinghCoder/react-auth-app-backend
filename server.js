e3764d129cb9e055e8db57e193b65e674329fc1b
AnuragsinghCoder <www.saury@gmail.com>
2026-09-14 18:29:00 +0530

    test vercel api routes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const userRouter = require("./routes/userRouter");
 const authRouter = require("./routes/authRouter");
 const cors = require("cors");
@@ -16,23 +15,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
-
-// Root route
 app.get("/", (req, res) => {
     res.send("Backend is running");
 });
 
-// Connect MongoDB
-connectDB()
-    .then(() => {
-        console.log("MongoDB connected");
-    })
-    .catch((error) => {
-        console.error("MongoDB connection error:", error);
-    });
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
 
-// Export Express app for Vercel
 module.exports = app;
